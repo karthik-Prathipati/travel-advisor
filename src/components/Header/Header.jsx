15bd67c5b3140baa0a2e90fe6ba080d19cbcd910
karthikpratihpati <karthikprathipati1@gmail.com>
2022-06-22 11:10:53 +0530

    created a basic file structure for the project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar,ToolBar,InputBase,Typography,Box } from "@mui/material";
+
+export default function Header(){
+    return (
+       <div>
+            <h1> Header</h1>
+       </div>
+            
+    );
+}
